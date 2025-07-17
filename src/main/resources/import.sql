2efda6a1088925d204defea951b12812af573377
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-07-17 19:37:35 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$82ysEuGFE7KQKslH.J5sEusrJhlhjWN8nVV3g6qYGoXAb.l28DPgK');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$82ysEuGFE7KQKslH.J5sEusrJhlhjWN8nVV3g6qYGoXAb.l28DPgK');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
